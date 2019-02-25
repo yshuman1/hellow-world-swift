14c2aaf55f85c74da0118101a57b61d9812c1f70
Yasin Shuman <email@yasin.io>
2019-02-25 03:56:53 -0800

    quick hello world swift app

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by yasin shuman on 2/25/19.
+//  Copyright © 2019 yasin shuman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
+
